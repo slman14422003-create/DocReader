5d1a56e7f6e3940e5f6f7ab7918113f707ae3367
slx23m <slman14422003@gmail.com>
2026-09-23 11:52:37 +0300

    Add files via upload

--- app/src/main/java/com/docreader/app/model/RecentFile.java
+++ app/src/main/java/com/docreader/app/model/RecentFile.java
@@ -1,1 +1,15 @@
+package com.docreader.app.model;
 
+public class RecentFile {
+    public String uri;
+    public String displayName;
+    public String mimeType;
+    public long lastOpened;
+
+    public RecentFile(String uri, String displayName, String mimeType, long lastOpened) {
+        this.uri = uri;
+        this.displayName = displayName;
+        this.mimeType = mimeType;
+        this.lastOpened = lastOpened;
+    }
+}
